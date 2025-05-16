156ab3c9734eb6e9e3c365b9e1808b5f94b46359
Juliana Roque <julianaroquedesouza@gmail.com>
2025-05-16 10:49:53 -0300

    Add files via upload

---
+++ matriz.py
@@ -0,0 +1,26 @@
+matriz=[ [0] * for - in range[4] ]
+#prenche alinha de indice 0 da matriz:
+#atribui o valor o ao elemento na linha 0, coluna o
+matriz[0] [0] =0
+#atribui o valor 1 ao elemento na linha 0,coluna 1
+matriz [0] =1 
+#atribui o valor 1 ao elemento na linha,0 coluna 2
+matriz [0][2]=0
+#Atribui o valos 1 ao elemento na linha 0 coluna 3
+        
+#Preenche a linha de indice 1 da matriz;
+#Atribui o valor 0 ao elemento na linha 1, coluna 0
+ 
+Matriz [1] [3] = 0
+ #Exibindo 0s valores da matriz,
+ #loop externo para percorrer os indica linha [de 0 a 3
+ For contador2 ir range[4]:
+# imprime o elemento da maztriz na linha e coluna [de 0 a 3
+For contador2 in range [4] :
+#Imprime o elemento da matriz na linha e columa atuais sem adicionar uma nova linha 
+
+print[Matriz][contador],end´"" ]
+#Imprime uma nova linha após a impressáo de todos os elementos de uma linha da 
+Matriz 
+print[ ]
+
